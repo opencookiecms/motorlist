49082fc02fe6a6b131cc0929f21588018e33cc3c
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2021-06-02 16:54:36 +0800

    Update models.py

--- src/rs150/models.py
+++ src/rs150/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+class MClient(models.Model):
+    pass
+class Motorshop(models.Model):
+    pass
+class Post(models.Model):
+    pass
+class Motorcycle(models.Model):
+    pass
+class Specfication(models.Model):
+    pass
+class Part(models.Model):
+    pass
+class Rating(models.Model):
+    pass
